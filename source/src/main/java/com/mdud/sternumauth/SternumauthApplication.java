262b311bccdea89c3d35818e3d1e62336aaa47d4
MDud <duda915@gmail.com>
2019-04-06 12:00:15 +0200

    spring init

---
+++ source/src/main/java/com/mdud/sternumauth/SternumauthApplication.java
@@ -0,0 +1,13 @@
+package com.mdud.sternumauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SternumauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SternumauthApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/mdud/sternumauth/SternumauthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mdud.sternumauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SternumauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
