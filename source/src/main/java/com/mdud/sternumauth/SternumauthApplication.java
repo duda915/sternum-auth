d26cf930d117ada2a33f77e9dfbf039adfcd2710
MDud <duda915@gmail.com>
2019-04-06 15:34:53 +0200

    fetching properties from sternum-config

--- source/src/main/java/com/mdud/sternumauth/SternumauthApplication.java
+++ source/src/main/java/com/mdud/sternumauth/SternumauthApplication.java
@@ -1,13 +1,13 @@
 package com.mdud.sternumauth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SternumauthApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SternumauthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SternumauthApplication.class, args);
+    }
 
 }
