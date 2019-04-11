4366fec52304416e5aa6015a1516f5a249a7a152
MDud <duda915@gmail.com>
2019-04-11 17:29:59 +0200

    approval page html

--- source/src/main/java/com/mdud/sternumauth/login/LoginController.java
+++ source/src/main/java/com/mdud/sternumauth/login/LoginController.java
@@ -2,7 +2,10 @@ package com.mdud.sternumauth.login;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -13,7 +16,8 @@ public class LoginController {
     }
 
     @GetMapping("/oauth/confirm_access")
-    public String confirmOAuthAccess() {
+    public String confirmOAuthAccess(Model model, @RequestParam("client_id") String clientId) {
+        model.addAttribute("appToAuth", clientId);
         return "approveoauth";
     }
 }
