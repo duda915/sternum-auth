f16cb4a6e84746e2b50ba308127e8be40a29b504
MDud <duda915@gmail.com>
2019-04-11 15:00:31 +0200

    static resources in template

--- source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
+++ source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/webjars/**", "/images/**", "/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
                 .and().logout().permitAll();
