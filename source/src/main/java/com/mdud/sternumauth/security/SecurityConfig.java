a2f899e1e8ca0041d6b15a17339e3b82d4a2aff5
MDud <duda915@gmail.com>
2019-04-11 14:29:04 +0200

    fixed webjars

--- source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
+++ source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
     }
 }
