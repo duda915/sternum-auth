1aa8686a691533d9a39d6a2a7a42d1eda171ed3b
MDud <duda915@gmail.com>
2019-04-12 13:51:32 +0200

    added spring mail component

---
+++ source/src/main/java/com/mdud/sternumauth/registration/mail/MailService.java
@@ -0,0 +1,5 @@
+package com.mdud.sternumauth.registration.mail;
+
+public interface MailService {
+    void sendMail(String mail);
+}
---
+++ source/src/main/java/com/mdud/sternumauth/registration/mail/MailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.mdud.sternumauth.registration.mail;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService {
+    @Override
+    public void sendMail(String mail) {
+
+    }
+}
