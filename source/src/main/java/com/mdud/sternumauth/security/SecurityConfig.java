d31b46c8751c8f51df76ce047ad745db5e723794
MDud <duda915@gmail.com>
2019-04-16 10:47:26 +0200

    fixed activation

--- source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
+++ source/src/main/java/com/mdud/sternumauth/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/webjars/**", "/images/**", "/css/**", "/register, /activate")
+                .antMatchers("/webjars/**", "/images/**", "/css/**", "/register", "/activate")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll()
